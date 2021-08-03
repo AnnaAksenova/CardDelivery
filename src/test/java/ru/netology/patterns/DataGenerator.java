c6692b6a3e15f3440e299684ef1f77c684d8098f
Anna Aksenova <annaa_aksenova@mail.ru>
2021-08-03 22:54:15 +0300

    New Format Phone

--- src/test/java/ru/netology/patterns/DataGenerator.java
+++ src/test/java/ru/netology/patterns/DataGenerator.java
@@ -24,7 +24,7 @@ public class DataGenerator {
 
     public static String generatePhone(String locale) {
         Faker faker = new Faker(new Locale(locale));
-        return faker.phoneNumber().phoneNumber();
+        return faker.numerify("+7##########");
     }
 
     public static String generateCity(String locale) {
